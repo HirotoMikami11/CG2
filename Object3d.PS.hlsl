a5d13f09959bc12989c508f75e09da2ee3a789af
LE2A_16_Mikami <146906574+HirotoMikami11@users.noreply.github.com>
2024-06-01 15:59:23 +0900

    a

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -1,26 +1,23 @@
 
 
-struct Material
+cbuffer Material : register(bo)
 {
-    float32_t4 color;
+    float4 color;
 };
-
-ConstantBuffer<Material> gMaterial : register(b0);
-
-//Textureの宣言
-//Textureは基本的にSamplerを介して読む=Samplingという
-Texture2D<float32_t4> gTexture : register(t0);
-SamplerState gSampler : register(s0);
-
 struct PixelShaderOutput
 {
-    float32_t4 color : SV_TARGET0;
+    float4 color : SV_TARGET0;
 };
+//Textureの宣言
+//Textureは基本的にSamplerを介して読む=Samplingという
+Texture2D<float4> gTexture : register(t0);
+SamplerState gSampler : register(s0);
+
 
 struct VertexShaderOutput
 {
-    float32_t4 position : SV_POSITION;
-    float32_t2 texcoord : TEXCOORD0;
+    float4 position : SV_POSITION;
+    float2 texcoord : TEXCOORD0;
 
 };
 
@@ -30,9 +27,9 @@ PixelShaderOutput main(VertexShaderOutput input)
     PixelShaderOutput output;
 
 
-    float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
+    float4 textureColor = gTexture.Sample(gSampler, input.texcoord);
     //サンプリングしたtextureの色とマテリアルん色を乗算して合成する
-    output.color = gMaterial.color * textureColor;
+    output.color = color * textureColor;
   
     return output;
 }
--- Object3d.VS.hlsl
+++ Object3d.VS.hlsl
@@ -1,26 +1,24 @@
-struct TransformationMatrix
+cbuffer TransformationMatrix : register(b0)
 {
-    float32_t4x4 WVP;
+    float4x4 WVP;
 };
-
-ConstantBuffer<TransformationMatrix> gTransformationMatrix : register(b0);
 
 struct VertexShaderOutput
 {
-    float32_t4 position : SV_POSITION;
-    float32_t2 texcoord : TEXCOORD0;
+    float4 position : SV_POSITION;
+    float2 texcoord : TEXCOORD0;
 };
 
 struct VertexShaderInput
 {
-    float32_t4 position : POSITION0;
-    float32_t2 texcoord : TEXCOORD0;
+    float4 position : POSITION0;
+    float2 texcoord : TEXCOORD0;
 };
 
 VertexShaderOutput main(VertexShaderInput input)
 {
     VertexShaderOutput output;
-    output.position = mul(input.position, gTransformationMatrix.WVP);
+    output.position = mul(input.position, WVP);
     output.texcoord = input.texcoord;
     return output;
 }
